a8d31eebf2d27133d412cf74344e245c2479c209
theody2002 <100077901+theody2002@users.noreply.github.com>
2022-02-20 22:00:58 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>My Home Page </title>
+  </head>
+  <body>
+  
+  <h1>Welcome to My Website! I am Theodore C. Bayalas ! </h1>
+  <ul>
+    <li><a href="index.php">HOME</a></li>
+    <li><a href="/LT2/index.html">LEARNING TASK 2</a></li>
+ 
+  </ul>
+  </body>
+</html>
+  
+    
+  
