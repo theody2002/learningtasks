02628f2e80796a1a2ec43f9485da1a7dd4fdbf00
theody2002 <100077901+theody2002@users.noreply.github.com>
2022-03-13 17:36:40 +0800

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,8 @@
   <ul>
     <li><a href="index.php">HOME</a></li>
     <li><a href="/LT2/index.html">LEARNING TASK 2</a></li>
-    <li><a hreft="/LT3/index.html">LEARNING TASK 3</a></li>
+    <li><a href="/LT3/index.html">LEARNING TASK 3</a></li>
+   
  
   </ul>
   </body>
