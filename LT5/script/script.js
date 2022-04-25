fbb6a006ee8328282aa963a3aaecf12ee352d7ba
theody2002 <100077901+theody2002@users.noreply.github.com>
2022-04-25 21:14:36 +0800

    Add files via upload

---
+++ LT5/script/script.js
@@ -0,0 +1,12 @@
+var name = prompt("Enter Your Name: ");
+alert("Hi " + name + ", Welcome and Enjoy!");
+alert("Welcome to Learning Task 5.1");
+
+function setNewImage ()
+{
+    document.getElementById("img1").src = "style/eva.jpg";
+}
+function setOldImage ()
+{
+    document.getElementById("img1").src = "style/shinji.jpg";
+}
