c8f586a26b07efed6d44bb3196585e250e30c748
theody2002 <100077901+theody2002@users.noreply.github.com>
2022-05-21 14:49:35 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,41 @@
+<?php
+$page = (isset($_GET['page']) && $_GET['page'] != '') ? $_GET['page'] : '';
+?>
 <!DOCTYPE html>
 <html>
+
 <head>
-  <title>My Home Page </title>
-  </head>
-  <body>
-  
-  <h1>Welcome to My Website! I am Theodore C. Bayalas ! </h1>
-  <ul>
-    <li><a href="index.php">HOME</a></li>
-    <li><a href="/LT2/index.html">LEARNING TASK 2</a></li>
-    <li><a href="/LT3/index.html">LEARNING TASK 3</a></li>
-    <li><a href="/LT4/index.html">LEARNING TASK 4</a></li>
-    <li><a href="/CLE1MT/index.html">LEARNING EVIDENCE</a></li>
-   
- 
-  </ul>
-  </body>
+    <title>index</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+
+</head>
+
+<body>
+    <header>
+        <div id="nav">
+            <div class="logo">
+                <h3>TB</h3>
+            </div>
+            <a href="index.php?page=About">About Me</a>
+            <a href="index.php?page=Works">My Works</a>
+            <a href="index.php">Home</a>
+
+            <?php
+            switch ($page) {
+                case "About":
+                    include "About.php";
+                    break;
+                case "Works":
+                    include "Works.php";
+                    break;
+                default:
+                    include "default.php";
+            }
+            ?>
+    </header>
+
+</body>
+
 </html>
-  
-    
-  
